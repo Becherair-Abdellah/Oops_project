bd01df79d876d9f16f0e3a0d4eded0991c50336c
abdellah becherair <96389488+Becherair-Abdellah@users.noreply.github.com>
2023-02-23 13:23:14 +0100

    Add files via upload

--- oops orginal v1.0.1/js file/main.js
+++ oops orginal v1.0.1/js file/main.js
@@ -269,13 +269,13 @@ function threedots() {
             </svg>`;
             title.innerHTML = 'save post';
             p.innerText = 'add this to your saved Items';
-            li.onclick = ()=>{
+            li.onclick = () => {
                 li.classList.toggle('chColor');
-                setTimeout(()=>{
-                li.classList.toggle('scale');
-                },10)
-                li.classList.contains('chColor')?title.innerHTML = 'saved post':title.innerHTML = 'save post';
-                li.classList.contains('chColor')?p.innerText = 'this post is added to your saved posts':p.innerText = 'add this to your saved Items';
+                setTimeout(() => {
+                    li.classList.toggle('scale');
+                }, 10)
+                li.classList.contains('chColor') ? title.innerHTML = 'saved post' : title.innerHTML = 'save post';
+                li.classList.contains('chColor') ? p.innerText = 'this post is added to your saved posts' : p.innerText = 'add this to your saved Items';
             }
         }
         if (i == 1) {
@@ -285,19 +285,32 @@ function threedots() {
             </svg>`;
             title.innerHTML = 'Report Post';
             p.innerText = `I'm concerned about this`;
-            li.onclick = ()=>{
+            li.onclick = () => {
                 li.classList.toggle('chColor');
-                setTimeout(()=>{
-                li.classList.toggle('scale');
-                },10)
-                li.classList.contains('chColor')?title.innerHTML = 'Reported post':title.innerHTML = 'Report Post';
-                li.classList.contains('chColor')?p.innerText = `I'm Not concerned about this`:p.innerText = `I'm concerned about this`;
+                setTimeout(() => {
+                    li.classList.toggle('scale');
+                }, 10)
+                li.classList.contains('chColor') ? title.innerHTML = 'Reported post' : title.innerHTML = 'Report Post';
+                li.classList.contains('chColor') ? p.innerText = `I'm Not concerned about this` : p.innerText = `I'm concerned about this`;
             }
         }
         if (i == 2) {
             spanLi.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm88 200H296c13.3 0 24 10.7 24 24s-10.7 24-24 24H152c-13.3 0-24-10.7-24-24s10.7-24 24-24z"/></svg>`;
             title.innerHTML = 'Hide Post';
             p.innerText = `See fewer Posts like this`;
+            li.onclick = () => {
+                // remove div dots
+                dots.classList.remove('move');
+                setTimeout(() => {
+                    dots.classList.remove('show');
+                    dots.remove();
+                    // remove post
+                    document.querySelector('.post').classList.add('move');
+                    back();
+                }, 400);
+                // create div back
+                body.classList.remove('noneScroll');
+            }
         }
         if (i == 3) {
             spanLi.innerHTML = `<?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
@@ -305,29 +318,32 @@ function threedots() {
             <path d="M19.53 8L14 2.47C13.8595 2.32931 13.6688 2.25018 13.47 2.25H11C10.2707 2.25 9.57118 2.53973 9.05546 3.05546C8.53973 3.57118 8.25 4.27065 8.25 5V6.25H7C6.27065 6.25 5.57118 6.53973 5.05546 7.05546C4.53973 7.57118 4.25 8.27065 4.25 9V19C4.25 19.7293 4.53973 20.4288 5.05546 20.9445C5.57118 21.4603 6.27065 21.75 7 21.75H14C14.7293 21.75 15.4288 21.4603 15.9445 20.9445C16.4603 20.4288 16.75 19.7293 16.75 19V17.75H17C17.7293 17.75 18.4288 17.4603 18.9445 16.9445C19.4603 16.4288 19.75 15.7293 19.75 15V8.5C19.7421 8.3116 19.6636 8.13309 19.53 8ZM14.25 4.81L17.19 7.75H14.25V4.81ZM15.25 19C15.25 19.3315 15.1183 19.6495 14.8839 19.8839C14.6495 20.1183 14.3315 20.25 14 20.25H7C6.66848 20.25 6.35054 20.1183 6.11612 19.8839C5.8817 19.6495 5.75 19.3315 5.75 19V9C5.75 8.66848 5.8817 8.35054 6.11612 8.11612C6.35054 7.8817 6.66848 7.75 7 7.75H8.25V15C8.25 15.7293 8.53973 16.4288 9.05546 16.9445C9.57118 17.4603 10.2707 17.75 11 17.75H15.25V19ZM17 16.25H11C10.6685 16.25 10.3505 16.1183 10.1161 15.8839C9.8817 15.6495 9.75 15.3315 9.75 15V5C9.75 4.66848 9.8817 4.35054 10.1161 4.11612C10.3505 3.8817 10.6685 3.75 11 3.75H12.75V8.5C12.7526 8.69811 12.8324 8.88737 12.9725 9.02747C13.1126 9.16756 13.3019 9.24741 13.5 9.25H18.25V15C18.25 15.3315 18.1183 15.6495 17.8839 15.8839C17.6495 16.1183 17.3315 16.25 17 16.25Z" fill="#000000"/>
             </svg>`;
             title.innerHTML = 'Copy link';
+            
+            li.onclick = () => {
+                li.classList.add('chColor');
+                setTimeout(() => {
+                    li.classList.add('scale');
+                }, 10);
+                title.innerHTML = 'Copied!';
+                copyText(window.location.href);
+            }
         }
     }
     dots.appendChild(content);
     slash.onclick = () => {
         dots.classList.remove('move');
         setTimeout(() => {
-            dots.classList.remove('.dots');
-        }, 0);
-        document.querySelector('body').classList.remove('noneScroll');
+            dots.classList.remove('show');
+            dots.remove();
+        }, 400);
+        body.classList.remove('noneScroll');
+
     }
     body.appendChild(dots);
     setTimeout(() => {
         dots.classList.add('move');
     }, 0)
     dots.classList.add('show');
-    document.querySelector('.container').onclick = (e) => {
-        if (e.target !== document.querySelector('.controlTopPost span:last-child svg')) {
-            dots.classList.remove('move');
-            setTimeout(() => {
-                dots.classList.remove('.dots');
-            }, 0)
-        }
-    }
     let slashArea = slash.parentNode.getBoundingClientRect().top;
     let positionY;
     let initY;
@@ -343,8 +359,9 @@ function threedots() {
             if (diffY > 0) {
                 dots.classList.remove('move');
                 setTimeout(() => {
-                    dots.classList.remove('.dots');
-                }, 0)
+                    dots.remove();
+                }, 400)
+                body.classList.remove('noneScroll');
             }
         });
     });
@@ -424,12 +441,61 @@ function createComments(parent) {
     parent.appendChild(commenter);
 }
 // function back 
-function back(){
-    let divBack = document.createElement('span');
-    divBack
+function back() {
+    // add time out to post div
+    let timeNone = setTimeout(() => {
+        document.querySelector('.post').classList.add('none');
+    }, 800)
+    let divBack = document.createElement('div');
+    divBack.classList = 'back';
+    // create text
+    let textBack = document.createElement('p');
+    textBack.innerText = 'this post is deleted successfuly';
+    // create btn back
+    let btnBack = document.createElement('button');
+    btnBack.id = 'back';
+    btnBack.innerText = 'back'
+    divBack.appendChild(textBack);
+    divBack.appendChild(btnBack);
+    body.appendChild(divBack);
+    setTimeout(() => {
+        divBack.classList.add('scale');
+    }, 0)
+    let timeOne = setTimeout(() => {
+        divBack.classList.remove('scale');
+        setTimeout(() => {
+            document.querySelector('.post').remove();
+            divBack.remove();
+        }, 500);
+    }, 4000);
+    btnBack.onclick = () => {
+        document.querySelector('.post').classList.remove('none');
+        setTimeout(() => {
+            document.querySelector('.post').classList.remove('move');
+        });
+        clearTimeout(timeOne);
+        clearTimeout(timeNone);
+        divBack.remove();
+    }
+
+}
+// function for copy any text or url
+function copyText(text) {
+    let input = document.createElement('input');
+    input.id = 'copy';
+    input.type = 'text';
+    input.readOnly = true;
+    input.value = `${text}`;
+    body.appendChild(input);
+        input.select();
+        document.execCommand('copy');
+        document.getSelection().removeAllRanges();
+        input.remove();
 }
 controlTop();
 Container();
+createPost();
+createPost();
 createPost();
 controlBottom();
 mainSelectors();
