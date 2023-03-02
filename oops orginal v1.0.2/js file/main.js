1c558cffa9b7f801337d578d96ced67599d34b4c
abdellah becherair <96389488+Becherair-Abdellah@users.noreply.github.com>
2023-03-02 08:05:02 +0100

    Add files via upload

--- oops orginal v1.0.2/js file/main.js
+++ oops orginal v1.0.2/js file/main.js
@@ -187,7 +187,7 @@ function createPost() {
     post.appendChild(comments);
     // console.log(document.querySelector());
     for (i = 0; i < 15; i++) {
-        createComments(allComments, 'abdellah behcerair mohmeassscshcjshc sjhcswk;');
+        createComments(allComments, 'abdellah becherair and tools foods main');
         nbrComments++;
     }
     // create div 'veiw comments ' 
@@ -547,18 +547,19 @@ function createComments(parent, text) {
     dots_Comments.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 256a56 56 0 1 1 112 0A56 56 0 1 1 0 256zm160 0a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zm216-56a56 56 0 1 1 0 112 56 56 0 1 1 0-112z"/></svg>`;
     // when user click at dots that is exist in comment run this function 'dotsComments'
     dots_Comments.onclick = (e) => {
-                // set prop at  div 'name_comment'
-                name_comment.style.zIndex = '1';
+        dots_Comments.style.pointerEvents = 'none';
+        // set prop at  div 'name_comment'
+        name_comment.style.zIndex = '1';
         if (document.querySelector('.hidden')) {
             document.querySelector('.hidden').parentNode.children[2].style.zIndex = '0';
-            document.querySelector('.hidden').classList.remove('hidden'); 
+            document.querySelector('.hidden').classList.remove('hidden');
         }
         // hidden div intract comment
         intract_comment.classList.add('hidden');
 
         // remove all created div dots comments
         document.querySelector('.fun-comment') ? document.querySelector('.fun-comment').remove() : '';
-        dotsComments(name_comment, dots_Comments,intract_comment);
+        dotsComments(name_comment, dots_Comments, intract_comment);
         // disable clicking
         setTimeout(() => {
             document.onclick = () => {
@@ -635,8 +636,8 @@ function copyText(text) {
     input.remove();
 }
 // function create dots of div 'comments'
-function dotsComments(parent, eleMain,elechange) {
-    
+function dotsComments(parent, eleMain, elechange) {
+    console.log(parent.parentNode.children[0]);
     // create div dots Comments 
     let div_dots = document.createElement('div');
     div_dots.classList = 'fun-comment';
@@ -649,17 +650,13 @@ function dotsComments(parent, eleMain,elechange) {
     li_edit.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.8 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg>`;
     // when click at edit run function 'editComments'
     li_edit.onclick = () => {
+        console.log(elechange);
         /* code of function to edit comment */
         // editComments(parent.children[3], parent.children[3].innerText);
         elechange.children[0].style = `display:none;`
         elechange.children[1].style = `display:none;`
         // change some prop in css
         elechange.classList.add('changed');
-        // chaek divs svae and cancel is not exist
-        if(document.querySelector('#save')&&document.querySelector('#cancel')){
-            document.querySelector('#save').remove();
-            document.querySelector('#cancel').remove();
-        }
         // create function edit comment
         let save = document.createElement('span');
         save.id = 'save';
@@ -669,19 +666,19 @@ function dotsComments(parent, eleMain,elechange) {
         cancel.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M368 368L144 144M368 144L144 368"/></svg>`;
         elechange.appendChild(save);
         elechange.appendChild(cancel);
-        // set document to set Time out is accept to run function that apply on document
-        setTimeout(()=>{
-            document.onclick = ()=>{
-                // remove none from last spans
-                elechange.children[0].style = `display:block;`;
-                elechange.children[1].style = `display:block;`;
-                // set none to new last span sva and cancel
-                save.style.display = 'none';
-                cancel.style.display = 'none';
-                // remove class changed
-                elechange.classList.remove('changed');
-            }
-        },0)
+        cancel.onclick = () => {
+            // remove none from last spans
+            elechange.children[0].style = `display:block;`;
+            elechange.children[1].style = `display:block;`;
+            // set none to new last span sva and cancel
+            save.style.display = 'none';
+            cancel.style.display = 'none';
+            // remove class changed
+            elechange.classList.remove('changed');
+            eleMain.style.pointerEvents = 'unset';
+        }
+        parent.children[3].style.display = 'none';
+        contentComment(parent,parent.children[3],elechange,save);
     }
     // delete function
     let li_delet = document.createElement('li');
@@ -699,45 +696,38 @@ function dotsComments(parent, eleMain,elechange) {
         eleMain.classList.remove('noneclick')
     }
 }
-// function create edit comments
-function editComments(parent, text) {
-    // set animation 
-    console.log(parent);
-    setTimeout(() => {
-        divEdit.classList.add('show');
-        body.classList.add('noneScroll');
-    }, 0);
-    let divEdit = document.createElement('div');
-    divEdit.classList = 'edit-comment';
+// function create div to edit comment
+function contentComment(nameComment,CommentPOst,intract,btnSave){
+    console.log(nameComment.parentNode.children[0])
     let textarea = document.createElement('textarea');
-    textarea.value = text;
-    setTimeout(() => {
-        textarea.focus();
-    }, 0)
-    textarea.placeholder = 'edit your comment...';
-    let btnRemove = document.createElement('span');
-    btnRemove.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><title>Close</title><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M368 368L144 144M368 144L144 368"/></svg>`;
-    let btnSend = document.createElement('button');
-    btnSend.innerText = 'SEND';
-    btnSend.id = 'send-comment';
-    // when user click at send change text of comments
-    btnSend.onclick = () => {
-        // trim for remove first and last spaces
-        if (textarea.value.trim() !== '') {
-            parent.innerText = textarea.value.trim();
-            divEdit.remove();
-        }
-
+    textarea.id = 'edit-comment';
+    nameComment.appendChild(textarea);
+    intract.style = 'bottom:-30px;'
+    textarea.focus();
+    textarea.innerText = CommentPOst.innerText;
+    textarea.addEventListener('keyup', (e) => {
+        textarea.style.height = 'auto';
+        textarea.style.height = `${e.target.scrollHeight}px`;
+    });
+    btnSave.onclick = ()=>{
+        CommentPOst.innerText = textarea.value;
+        CommentPOst.style.display = 'block';
+        intract.style = 'bottom:-22px;'
+        nameComment.children[0].style.pointerEvents = 'unset';
+        nameComment.children.parentNode.children[0].style = `display:block;`;
+        nameComment.children.parentNode.children[1].style = `display:block;`;
+        // set none to new last span sva and cancel
+        btnSave.style.display = 'none';
+        // cancel.style.display = 'none';
+        // remove class changed
+        // elechange.classList.remove('changed');
+        // eleMain.style.pointerEvents = 'unset';
+        textarea.remove();
     }
-    // append
-    divEdit.appendChild(textarea);
-    divEdit.appendChild(btnRemove);
-    divEdit.appendChild(btnSend);
-    body.appendChild(divEdit);
 }
 controlTop();
 Container();
-    createPost();
-    createPost();
+createPost();
+createPost();
 controlBottom();
 mainSelectors();
